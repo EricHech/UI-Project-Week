75c4a865e7ca6bd7e5d23b7b978ddfb4cb13141b
Eric <erichech@gmail.com>
2018-07-18 16:30:46 -0400

    making progress on services page and getting the tabs to work

--- js/index.js
+++ js/index.js
@@ -2,11 +2,49 @@ const navBtn = document.querySelector('.menu .navbar img');
 const menu = document.querySelector('.menu-items');
 const menuItems = document.querySelectorAll('.menu-items li');
 
-const menuTween = new TimelineMax({paused:true, reversed:true});
-menuTween.from(menu, .25, {height: 0, ease: Power2.easeOut, opacity: 0});
-menuTween.staggerFrom(menuItems, .2, {y: -20, opacity: 0}, .015);
+const menuTween = new TimelineMax({ paused: true, reversed: true });
+menuTween.from(menu, 0.25, { height: 0, ease: Power2.easeOut, opacity: 0 });
+menuTween.staggerFrom(menuItems, 0.2, { y: -20, opacity: 0 }, 0.015);
 
 navBtn.addEventListener('click', () => {
   menu.classList.toggle('menu-items_revealed');
   menuTween.reversed() ? menuTween.play() : menuTween.reverse();
 });
+
+//--------------------------------------------------------------------
+
+class Tabs {
+  constructor (element) {
+    this.element = element;
+    this.tabs = this.element.map((eachTab) => new TabItem(eachTab, this));
+    this.active = this.tabs[0].element;
+    this.init();
+  }
+
+  init () {
+    console.log(this.active);
+    this.active.classList.add("tab_selected");
+  }
+
+  updateActive(activate) {
+    this.active.classList.remove('tab_selected');
+    this.active = activate;
+    this.active.classList.add("tab_selected");
+  }
+}
+
+class TabItem {
+  constructor(element, parent) {
+    this.element = element;
+    this.parent = parent;
+    this.element.addEventListener('click', this.onClick.bind(this));
+  }
+
+  onClick() {
+    this.parent.updateActive(this.element);
+  }
+}
+
+const tabs = new Tabs(Array.from(document.querySelectorAll('.tabs .each-tab')));
+const tabContent = document.querySelectorAll('.tabs .tab-content');
+console.log(tabContent);
