0078174be89a775e5b892495de006c6daeab0a69
Eric <erichech@gmail.com>
2018-07-18 13:10:21 -0400

    built the drop-down menu
    - created the dropdown
    - added gsap animations

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// JS goes here
+const navBtn = document.querySelector('.menu .navbar img');
+const menu = document.querySelector('.menu-items');
+const menuItems = document.querySelectorAll('.menu-items li');
+
+const menuTween = new TimelineMax({paused:true, reversed:true});
+menuTween.from(menu, .25, {height: 0, ease: Power2.easeOut, opacity: 0});
+menuTween.staggerFrom(menuItems, .2, {y: -20, opacity: 0}, .015);
+
+navBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu-items_revealed');
+  menuTween.reversed() ? menuTween.play() : menuTween.reverse();
+});
